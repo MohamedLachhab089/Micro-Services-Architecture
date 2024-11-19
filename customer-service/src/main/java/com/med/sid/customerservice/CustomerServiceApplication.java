adc1814956a38bab35de1728b72716b4c34407af
Mohamed <mohamed.lachhab@emsi-edu.ma>
2024-11-20 00:01:39 +0100

    add config part

--- customer-service/src/main/java/com/med/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/med/sid/customerservice/CustomerServiceApplication.java
@@ -1,16 +1,19 @@
 package com.med.sid.customerservice;
 
+import com.med.sid.customerservice.config.CustomerConfigParams;
 import com.med.sid.customerservice.entites.Customer;
 import com.med.sid.customerservice.repositories.CustomerRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
@@ -18,7 +21,7 @@ public class CustomerServiceApplication {
     }
 
     @Bean
-    public CommandLineRunner run(CustomerRepo customerRepo, RepositoryRestConfiguration restConfig) {
+    CommandLineRunner run(CustomerRepo customerRepo, RepositoryRestConfiguration restConfig) {
         restConfig.exposeIdsFor(Customer.class);
         return args -> {
             customerRepo.saveAll(List.of(
